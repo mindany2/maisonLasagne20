eb39bb9a6c35f81190ea8e9ade1fd9b1f72ba439
pi_appart <leo.dupontrenoux@gmail.com>
2021-11-01 10:54:18 +0100

    change setup, and data

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 git submodule update --init
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 sudo pip3 install -r requirements.txt
 sudo pip3 install spotipy
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
-sudo apt-get install supervisor
-sudo apt-get install apache2
-sudo apt-get install python3-smbus
+sudo apt-get -y install supervisor
+sudo apt-get -y install apache2
+sudo apt-get -y install python3-smbus
 sudo a2enmod wsgi
-sudo apt-get install libatlas-base-dev
-sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
-sudo cp utils/dev_file/site_maison.conf /etc/apache2/sites-enabled/
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install python3-pip apache2 libapache2-mod-wsgi-py3
+sudo cp dev_file/site_maison.conf /etc/apache2/sites-enabled/
 sudo rm /etc/apache2/sites-enabled/000-default.conf
-sudo cp utils/dev_file/supervisor/maison.conf /etc/supervisor/conf.d/
-sudo cp utils/dev_file/raspotify /etc/default/raspotify
+sudo cp dev_file/supervisor/maison.conf /etc/supervisor/conf.d/
+sudo cp dev_file/raspotify /etc/default/raspotify
+mkdir logs
 echo "now reboot and it is ok"
